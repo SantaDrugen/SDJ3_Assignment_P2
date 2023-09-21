af608761128d7296dd6a328bb34dc328174937df
SantaDrugen <emil0510@hotmail.com>
2023-09-21 10:41:09 +0200

    Create

---
+++ src/main/java/com/sdj3assignment/sdj3_assignment_p2/Sdj3AssignmentP2Application.java
@@ -0,0 +1,13 @@
+package com.sdj3assignment.sdj3_assignment_p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdj3AssignmentP2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdj3AssignmentP2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdj3assignment/sdj3_assignment_p2/Sdj3AssignmentP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdj3assignment.sdj3_assignment_p2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdj3AssignmentP2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
